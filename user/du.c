446b79945ff770797236ab403460a490a39800dd
anubis-robot <anubis@osiris.cyber.nyu.edu>
2022-09-12 22:46:22 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/du.c
+++ user/du.c
@@ -41,6 +41,7 @@ void du(char *path) {
   switch (st.type) {
   case T_FILE:
     printf(1, "%d %s\n", st.size, fmtname(path));
+    printf(1,"%d .\n",totalsize);
     break;
 
   case T_DIR:
@@ -67,7 +68,6 @@ void du(char *path) {
     }
     break;
   }
-  printf(1,"%d .\n",totalsize);
   close(fd);
 }
 
