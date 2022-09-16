76b3d127a4d74dd96b7c33c8409dd621ed6671c4
anubis-robot <anubis@anubis-lms.io>
2022-09-16 06:02:33 +0000

    Anubis Cloud IDE Autosave netid=ank8821

--- user/myfuntions.h
+++ user/myfuntions.h
@@ -45,3 +45,46 @@ char* strcat(char* s1, const char* s2)
 //     // the destination is returned by standard `strcat()`
 //     return destination;
 // }
+
+char* strstr(const char* string, const char* substring)
+{
+	const char *a, *b;
+
+	/* First scan quickly through the two strings looking for a
+	 * single-character match.  When it's found, then compare the
+	 * rest of the substring.
+	 */
+
+	b = substring;
+
+	if(*b == 0)
+	{
+		return (char*)string;
+	}
+
+	for(; *string != 0; string += 1)
+	{
+		if(*string != *b)
+		{
+			continue;
+		}
+
+		a = string;
+
+		while(1)
+		{
+			if(*b == 0)
+			{
+				return (char*)string;
+			}
+			if(*a++ != *b++)
+			{
+				break;
+			}
+		}
+
+		b = substring;
+	}
+
+	return ;
+}
